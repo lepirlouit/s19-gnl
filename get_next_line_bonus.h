7daf8aca00e994491ac682fdc0fded145eb213e0
Benoît de Biolley <bdb@aloalto.com>
2022-07-30 18:04:58 +0200

    add bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-biol <bde-biol@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 17:20:08 by bde-biol          #+#    #+#             */
+/*   Updated: 2022/07/30 18:12:36 by bde-biol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define MAX_OPEN_FILES 4096
+
+char	*get_next_line(int fd);
+size_t	ft_strlcpy(char *dst, const char *src,
+			size_t dstsize);
+ssize_t	ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*malloc_empty_string(void);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*join_and_free_s1(char *s1, char*S2);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+#endif
