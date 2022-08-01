16e294ef3d8fee1663d321f20f398fd376a87c4d
Benoît de Biolley <bdb@aloalto.com>
2022-08-01 17:26:28 +0200

    :seedling: remove softlink replaec with file copy

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,1 +1,111 @@
-get_next_line.c
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-biol <bde-biol@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 17:20:20 by bde-biol          #+#    #+#             */
+/*   Updated: 2022/07/31 22:46:54 by bde-biol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	size;
+
+	size = 0;
+	while (str[size])
+		size++;
+	return (size);
+}
+
+ssize_t	ft_strchr(const char *s, int c)
+{
+	ssize_t	position;
+
+	position = 0;
+	while (s[position])
+	{
+		if ((char)s[position] == (char)c)
+			return (position);
+		position++;
+	}
+	if (c == 0)
+		return (position);
+	return (-1);
+}
+
+ssize_t	concat_and_read(int fd, char **file_content)
+{
+	char	*buffer;
+	ssize_t	number_of_bytes_read;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (0);
+	number_of_bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (number_of_bytes_read == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[number_of_bytes_read] = 0;
+	*file_content = join_and_free_s1(*file_content, buffer);
+	if (!*file_content)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (number_of_bytes_read);
+}
+
+char	*replace_content_and_return(char **file_content)
+{
+	ssize_t	position;
+	char	*line;
+	char	*temp;
+
+	position = ft_strchr(*file_content, '\n');
+	if (position == -1)
+	{
+		line = ft_substr(*file_content, 0, ft_strlen(*file_content));
+		free(*file_content);
+		*file_content = NULL;
+		return (line);
+	}
+	line = ft_substr(*file_content, 0, position + 1);
+	temp = *file_content;
+	*file_content = ft_substr(*file_content, position + 1,
+			ft_strlen(*file_content) - position - 1);
+	free(temp);
+	return (line);
+}
+
+/*while file content doesn't contain \n and read from file is > 0*/
+char	*get_next_line(int fd)
+{
+	static char	*file_content[MAX_OPEN_FILES];
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
+		return (NULL);
+	if (!file_content[fd])
+		file_content[fd] = malloc_empty_string();
+	if (!file_content[fd])
+		return (NULL);
+	while (ft_strchr(file_content[fd], '\n') == -1
+		&& concat_and_read(fd, &file_content[fd]) > 0)
+		;
+	if (!file_content[fd])
+		return (NULL);
+	if (!ft_strlen(file_content[fd]))
+	{
+		free(file_content[fd]);
+		file_content[fd] = NULL;
+		return (NULL);
+	}
+	return (replace_content_and_return(&file_content[fd]));
+}
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,1 +1,102 @@
-get_next_line_utils.c
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-biol <bde-biol@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 17:20:24 by bde-biol          #+#    #+#             */
+/*   Updated: 2022/07/30 17:56:21 by bde-biol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlcpy(char *dst, const char *src,
+			size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	while (i < dstsize - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (i < dstsize)
+		dst[i] = '\0';
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*malloc_empty_string(void)
+{
+	char	*str;
+
+	str = malloc(sizeof(char));
+	if (!str)
+		return (NULL);
+	str[0] = 0;
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	max_len;
+
+	if (start > ft_strlen(s))
+		return (malloc_empty_string());
+	max_len = ft_strlen(s + start);
+	if (len < max_len)
+		max_len = len;
+	ptr = malloc(max_len + 1);
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, s + start, max_len + 1);
+	return (ptr);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		size;
+
+	size = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (!str || !s1 || !s2)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[size] = '\0';
+	return (str);
+}
+
+char	*join_and_free_s1(char *s1, char *s2)
+{
+	char	*new_str;
+
+	new_str = ft_strjoin(s1, s2);
+	free(s1);
+	return (new_str);
+}
